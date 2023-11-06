2fcd3a4bbd5ef051b6b2a19eacaef971d88216c1
Nguyễn Tấn Phát <tnpt003@gmail.com>
2023-11-06 15:31:34 +0700

    Add project files.

---
+++ WebApplication2/Scripts/FRONTEND/Slider.js
@@ -0,0 +1,21 @@
+﻿let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    if (index >= 0 && index < slides.length) {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = index;
+        slides[currentSlide].classList.add('active');
+    }
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Automatically advance the slider
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
